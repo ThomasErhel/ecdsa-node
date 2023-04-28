abc2b1b9984721dd858270256234524c69453bc5
Thomas Erhel <thomas.erhel@gmail.com>
2023-04-28 09:10:56 +0200

    🔒 Practice makes perfect

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const { recoverPublicKey } = require("ethereum-cryptography/secp256k1");
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0396a56bcabd859fc0ae920af3f03439d0ed471b263be17ee7083a6c3390130707": 100, // Mallory
+  "02dde78c4e9065cdba42cc8232a01e89d8cc34b2db033168515a445da366484196": 50, // Alice
+  "033473306d84b0cc9ff894e574f595c4f6ca0ed1ea73504111a2b4941db05e137b": 75, // Bob
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +20,21 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, signature, messageHash } = req.body;
+
+  try {
+    const publicKey = recoverPublicKey(
+      Buffer.from(messageHash, "hex"),
+      signature
+    );
+    const senderAddress = publicKeyToAddress(publicKey);
+
+    if (sender !== senderAddress) {
+      return res.status(400).send({ message: "Invalid signature!" });
+    }
+  } catch (err) {
+    return res.status(400).send({ message: "Invalid signature!" });
+  }
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
